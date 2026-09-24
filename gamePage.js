8bfcb1c6430d0ad1625b273f9dcd358bf3cc60aa
LR08-2 <162645498+LR08-2@users.noreply.github.com>
2026-09-24 19:48:33 +1000

    finish slides
    add slide animations

--- gamePage.js
+++ gamePage.js
@@ -1,13 +1,47 @@
 console.log("conected")
 
-var slideIndex =0
+var slideIndex = 0
 var slides = document.getElementsByClassName("slide")
 showSlide(0)
 
-function showSlide( newIndex ) {
-slideIndex = newIndex;
-Array.from(slides).forEach(element => {
-    element.style.display = "none"
-});
-slides[slideIndex].style.display = "block"
+function showSlide(newIndex) {
+    slideIndex = (newIndex + slides.length) % slides.length;
+    Array.from(slides).forEach(element => {
+        element.style.display = "none"
+    });
+    slides[slideIndex].style.display = "block"
+}
+
+function showSlideWithAnimation(newIndex) {
+    Array.from(slides).forEach(element => {
+        element.style.display = "none"
+    });
+    animateSlideOut(slides[slideIndex])
+    slideIndex = (newIndex + slides.length) % slides.length;
+    animateSlideIn(slides[slideIndex])
+}
+function changeSlide(by) {
+    showSlideWithAnimation(slideIndex + by)
+}
+
+function animateSlideIn(slide) {
+    slide.style.display = 'block'
+    slide.animate(
+        [
+            { transform: 'translateX(-500px)' },
+            { transform: 'translateX(0px)' }
+        ],
+        { duration: 400 })
+}
+
+function animateSlideOut(slide) {
+    slide.style.display = 'block'
+    var thisAnimation = slide.animate(
+        [
+            { transform: 'translateX(0)' },
+            { transform: 'translateX(500px)' }
+        ],
+        { duration: 400 }
+    )
+    thisAnimation.finished.then(function () { slide.style.display = 'none' })
 }
